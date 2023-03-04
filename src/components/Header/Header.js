945fcb4224d1022c0778434818ffa0a80523e65b
ronald-it <ronald.cirgofer@hotmail.com>
2023-03-04 20:34:36 +0100

    Gradient en box-shadow af, navlinks nog niet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,23 @@ import * as React from 'react';
 import styles from './Header.module.scss';
 import {Button} from "../Button/Button";
 import squiggle from '../../assets/squiggle_line_stripe.svg'
+import {NavLink} from "react-router-dom";
+import {Information} from "../Main/Information/Information";
 
 export function Header() {
     return (
         <header>
             <nav>
                 <ul>
-                    <li>case studies</li>
+                    <li>
+                        <NavLink to="/">case studies</NavLink>
+                    </li>
                     <li>services</li>
                     <li>contact us</li>
                 </ul>
             </nav>
             <h1>your website is an investment in the success of your business.</h1>
-            <h3>we are here to help you succeed.</h3>
+            <span>we are here to help you succeed.</span>
             <Button>let's work together</Button>
             <img src={squiggle} alt="squiggle line"/>
         </header>
