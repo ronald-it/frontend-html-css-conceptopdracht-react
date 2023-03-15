ea5ff083050a97bb392b6da486f6715a39070add
ronald-it <ronald.cirgofer@hotmail.com>
2023-03-15 19:03:03 +0100

    De squiggly line afbeeldingen uit de HTML gehaald en als pseudo-element geplaatst onder de project titels

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import styles from "../Project/Project.module.scss";
-import squiggle from "../../assets/squiggle_line.svg";
-
 
 export function Project({image, title, text}) {
     return (
@@ -11,7 +9,7 @@ export function Project({image, title, text}) {
             </span>
             <div className={styles['projects-div']}>
                 <h2 className={styles['article-title']}>{title}</h2>
-                <img className={styles['squiggle-line']} src={squiggle} alt="Squiggle line"/>
+                {/*<img className={styles['squiggle-line']} src={squiggle} alt="Squiggle line"/>*/}
                 <p className={styles['article-text']}>{text}</p>
                 <a className={styles['project-link']} href="https://www.google.com">view project</a>
             </div>
